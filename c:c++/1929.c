5efb6d3d1441c34692505d31bb9cf4f69d28c44f
JungYoonShin <sju08227@naver.com>
2021-01-26 20:59:30 +0900

    [Baekjoon] 1929번 코드

---
+++ c:c++/1929.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //에라토스테네스의 체
+    int n, m;
+    int num[1000001]={0,};
+    num[1]=1;
+    scanf("%d %d", &m, &n);
+    for(int i=2; i<=n; i++){
+        if(num[i]==1){
+            continue;
+        }
+        for(int j=i*i; j<=n; j+=i){
+            if(j%i==0){
+                num[j]=1;
+            }
+        }
+    }
+    for(int i=m; i<=n; i++){
+        if(num[i]==0){
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
